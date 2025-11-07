a351e6470293d8be1de06412651ab60739dc2d09
Dimas Rizki Ardiansyah <dimasrizkia477@gmail.com>
2025-11-07 22:02:09 +0700

    Update debug.php

--- public/debug.php
+++ public/debug.php
@@ -2,10 +2,13 @@
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Enable detailed errors
-$app->make('config')->set('app.debug', true);
-
 try {
+    // Clear config cache
+    $app->forgetInstance('config');
+    
+    // Manual set debug mode
+    putenv('APP_DEBUG=true');
+    
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
     $request = Illuminate\Http\Request::create('/');
     $response = $kernel->handle($request);
