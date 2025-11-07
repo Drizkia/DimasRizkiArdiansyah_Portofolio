025e8c9749403a361bcc47fe9ad2c93585db91ad
Dimas Rizki Ardiansyah <dimasrizkia477@gmail.com>
2025-11-07 21:47:39 +0700

    Update debug.php

--- public/debug.php
+++ public/debug.php
@@ -1,16 +1,13 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
-echo "LARAVEL BOOTSTRAP SUCCESS";
 
-include __DIR__.'/../vendor/autoload.php';
-echo "AUTOLOAD SUCCESS";
-
-include __DIR__.'/../vendor/autoload.php';
-$app = include_once __DIR__.'/../bootstrap/app.php';
-echo "APP BOOTSTRAP SUCCESS";
-
-include __DIR__.'/../vendor/autoload.php';
-$app = include_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-echo "KERNEL SUCCESS";
+try {
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    echo "KERNEL SUCCESS";
+} catch (Throwable $e) {
+    echo "KERNEL ERROR: " . $e->getMessage() . "\n";
+    echo "FILE: " . $e->getFile() . "\n"; 
+    echo "LINE: " . $e->getLine() . "\n";
+    echo "TRACE: " . $e->getTraceAsString();
+}
