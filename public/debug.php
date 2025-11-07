2f2087492d90b6cfd4c153e818b4181a868f6c63
Dimas Rizki Ardiansyah <dimasrizkia477@gmail.com>
2025-11-07 21:40:43 +0700

    Refactor debug.php to include autoload and bootstrap

--- public/debug.php
+++ public/debug.php
@@ -2,3 +2,15 @@
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 echo "LARAVEL BOOTSTRAP SUCCESS";
+
+include __DIR__.'/../vendor/autoload.php';
+echo "AUTOLOAD SUCCESS";
+
+include __DIR__.'/../vendor/autoload.php';
+$app = include_once __DIR__.'/../bootstrap/app.php';
+echo "APP BOOTSTRAP SUCCESS";
+
+include __DIR__.'/../vendor/autoload.php';
+$app = include_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+echo "KERNEL SUCCESS";
